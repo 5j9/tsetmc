82860b38d8bdd17672fb962c6012eb50d78e9f2a
5j9 <5j9@users.noreply.github.com>
2021-03-04 19:23:08 +0330

    release: v0.14.0

--- tsetmc/__init__.py
+++ tsetmc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.13.2.dev0'
+__version__ = '0.14.0'
 
 from ._core import Instrument, get_market_watch_init, get_closing_price_all, \
     get_client_type_all, get_key_stats, KNOWN_IDS
