3029ed351402836c6620347b390ba6d2f8493c8b
5j9 <5j9@users.noreply.github.com>
2021-10-18 16:08:41 +0330

    release: v0.29.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import dirname
 
 setup(
     name='tsetmc',
-    version='0.28.1.dev0',
+    version='0.29.0',
     long_description=open(
         f'{dirname(__file__)}/README.rst', encoding='utf-8').read(),
     long_description_content_type='text/x-rst',
