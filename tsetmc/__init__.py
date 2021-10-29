9de16f70175611df33a25f79753cb54a01194504
5j9 <5j9@users.noreply.github.com>
2021-10-29 12:49:46 +0330

    release: v0.31.1.dev0

--- tsetmc/__init__.py
+++ tsetmc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.31.0'
+__version__ = '0.31.1.dev0'
 
 from ._tsetmc import Instrument, market_watch_init, closing_price_all, \
     client_type_all, key_stats, L18S, price_adjustments, search
