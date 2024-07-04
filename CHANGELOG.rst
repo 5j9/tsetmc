8c34628b842de61ff1fa44ccdab6213c9557a742
5j9 <5j9@users.noreply.github.com>
2024-07-04 17:58:56 +0330

    release: v0.65.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,5 +1,5 @@
-Unreleased
-----------
+v0.65.0
+-------
 * BREAKING: All ETF/fund related functions were moved from ``general`` to ``funds`` module.
 
 v0.64.1
--- tsetmc/__init__.py
+++ tsetmc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.64.2.dev0'
+__version__ = '0.65.0'
 from enum import StrEnum as _StrEnum
 from functools import partial as _partial
 from json import JSONDecodeError, loads
